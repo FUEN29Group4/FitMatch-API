02b95696f6171d90dfa1f84587a368c9d58c7deb
RoryFong <asas10407@gmail.com>
2023-09-11 11:31:37 +0800

    新增Login

---
+++ FitMatch-API/Controllers/LoginController.cs
@@ -0,0 +1,118 @@
+﻿using Dapper;
+using FitMatch_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.AspNetCore.Identity;
+using Fluent.Infrastructure.FluentModel;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FitMatch_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // 網址 :  api/Member
+
+
+        private readonly IDbConnection _db;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _db = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            const string sql = @"SELECT * FROM Member";
+
+            using (var multi = await _db.QueryMultipleAsync(sql))
+            {
+                var Member = multi.Read<Member>().ToList();
+                // 基本驗證，確保資料存在
+                if (Member == null)
+                {
+                    return NotFound("No data found");
+                }
+                return Ok(Member);
+            }
+        }
+
+        // GET api/<MemberController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMember(int id)
+        {
+            const string sql = @"SELECT MemberId,Email,Password FROM Member WHERE MemberId = @MemberId";
+            var parameters = new { MemberId = id };
+
+            using (var multi = await _db.QueryMultipleAsync(sql, parameters))
+            {
+                var member = multi.Read<Member>().FirstOrDefault();
+                // 基本驗證，確保資料存在
+                if (member == null)
+                {
+                    return NotFound("No data found");
+                }
+                return Ok(member);
+            }
+        }
+
+
+
+
+
+
+
+
+
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // ... 登錄和註冊的實現
+    }
+
+
+
+    // GET: api/<LoginController>
+    //[HttpGet]
+    //public IEnumerable<string> Get()
+    //{
+    //    return new string[] { "value1", "value2" };
+    //}
+
+    // GET api/<LoginController>/5
+    //[HttpGet("{id}")]
+    //public string Get(int id)
+    //{
+    //    return "value";
+    //}
+
+    // POST api/<LoginController>
+    [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LoginController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LoginController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
