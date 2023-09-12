6c946520061f13f9379c948bbb9de9c33a3b6ed8
CyreneTsai <135968857+CyreneTsai@users.noreply.github.com>
2023-09-12 16:15:56 +0800

    G老師亂交的一些東西要修修

--- FitMatch-API/Controllers/ProductAPIController.cs
+++ FitMatch-API/Controllers/ProductAPIController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using FitMatch_API.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,37 +15,80 @@ namespace FitMatch_API.Controllers
     [Route("api/[controller]")]
     public class ProductAPIController : Controller
     {
-        // GET: api/values
+
+
+        //======宣告變數跟串資料庫=======
+
+        private readonly IDbConnection _context;//宣吿類別級變數，串資料庫
+        private readonly ILogger<OrderAPIController> _logger; //宣吿類別級變數，串登入資訊
+
+        //定義_context
+        public ProductAPIController(IConfiguration configuration)
+        {
+            _context = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> Products()
         {
-            return new string[] { "value1", "value2" };
+            const string sql = @"SELECT * FROM Product";
+
+            using (var multi = await _context.QueryMultipleAsync(sql))
+            {
+                var products = multi.Read<Product>().ToList();
+                // 基本驗證，確保資料存在
+                if (products == null)
+                {
+                    return NotFound("No data found");
+                }
+                return Ok(products);
+            }
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+
+
+
+
+
+
+
+
+
+        //// GET: api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
 
--- FitMatch-API/Models/OrderDetail.cs
+++ FitMatch-API/Models/OrderDetail.cs
@@ -6,13 +6,13 @@ namespace FitMatch_API.Models
 {
     public partial class OrderDetail
     {
-        //public int OrderDetailId { get; set; }//訂單明細編號
+        public int OrderDetailId { get; set; }//訂單明細編號
 
-        //public int OrderId { get; set; }//訂單編號
+        public int OrderId { get; set; }//訂單編號
 
-        //public int? ProductId { get; set; }//產品編號
+        public int? ProductId { get; set; }//產品編號
 
-        //public int? Quantity { get; set; }//數量
+        public int? Quantity { get; set; }//數量
 
     }
 }
