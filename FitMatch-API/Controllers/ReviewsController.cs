315a4fd7226ae3b45fca8300d3759a46f5bf6a95
Air505 <fuen29wei@gmail.com>
2023-09-13 09:44:53 +0800

    更新

--- FitMatch-API/Controllers/ReviewsController.cs
+++ FitMatch-API/Controllers/ReviewsController.cs
@@ -3,6 +3,7 @@ using System.Data.SqlClient;
 using System.Data;
 using Dapper;
 using FitMatch_API.Models;
+using System.Security.Claims;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -26,22 +27,65 @@ namespace FitMatch_API.Controllers
 
 
         // GET: api/<ReviewsController>
-        [HttpGet]
-        public async Task<IActionResult> GetALLMemberFavorite()
-        {
-            const string sql = @"SELECT * FROM [Reviews]";
+  //      [HttpGet]
+  //      public async Task<IActionResult> GetALLMemberFavorite()
+  //      {
+  //          const string sql = @"SELECT * FROM [Reviews]";
 
-            using (var multi = await _context.QueryMultipleAsync(sql))
-            {
-                var Reviewss = multi.Read<Reviews>().ToList();
-                // 基本驗證，確保資料存在
-                if (Reviewss == null)
-                {
-                    return NotFound("No data found");
-                }
-                return Ok(Reviewss);
-            }
-        }
+  //          using (var multi = await _context.QueryMultipleAsync(sql))
+  //          {
+  //              var Reviewss = multi.Read<Reviews>().ToList();
+  //              // 基本驗證，確保資料存在
+  //              if (Reviewss == null)
+  //              {
+  //                  return NotFound("No data found");
+  //              }
+  //              return Ok(Reviewss);
+  //          }
+  //      }
+
+  //      SELECT
+  //  r.ReviewID,
+  //  r.MemberID,
+  //  r.ProductID,
+  //  r.ClassID,
+  //  r.Rating,
+  //  r.Comment,
+  //  r.ReviewDateTime,
+  //  r.TrainerID,
+  //  r.ClassTypeID,
+
+  //  m.MemberID,
+  //  m.MemberName,
+
+  //  p.ProductID,
+  //  p.ProductName,
+
+  //  c.ClassID,
+  //  c.CourseStatus,
+
+  //  t.TrainerID,
+  //  t.TrainerName,
+
+  //  ct.ClassTypeID,
+  //  ct.ClassName
+
+  //FROM Reviews as r
+
+  //LEFT JOIN[Member] as m ON r.MemberID = m.MemberID
+
+  //LEFT JOIN Product as p ON r.ProductID = p.ProductID
+
+  //LEFT JOIN Class as c ON r.ClassID = c.ClassID
+
+  //LEFT JOIN Trainers as t ON r.TrainerID = t.TrainerID
+
+  //LEFT JOIN ClassTypes as ct ON r.ClassTypeID = ct.ClassTypeID
+
+
+  //WHERE r.MemberID = 3;
+
+
 
         // GET api/<ReviewsController>/5
         [HttpGet("{id}")]
