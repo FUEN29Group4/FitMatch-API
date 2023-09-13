8d156abd704a93f3476f3fcf8ce1e52b16c9bac7
Air505 <fuen29wei@gmail.com>
2023-09-13 11:44:49 +0800

    更新ReviewsAPI

--- FitMatch-API/Controllers/ReviewsController.cs
+++ FitMatch-API/Controllers/ReviewsController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
-using System.Data;
 using Dapper;
 using FitMatch_API.Models;
-using System.Security.Claims;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Data;
 
 namespace FitMatch_API.Controllers
 {
@@ -13,103 +14,100 @@ namespace FitMatch_API.Controllers
     [ApiController]
     public class ReviewsController : ControllerBase
     {
+        private readonly IDbConnection _context;
+        private readonly ILogger<ReviewsController> _logger;
 
-        //======宣告變數跟串資料庫=======
-
-        private readonly IDbConnection _context;//宣吿類別級變數，串資料庫
-        private readonly ILogger<ReviewsController> _logger; //宣吿類別級變數，串登入資訊
-
-        //定義_context
         public ReviewsController(IConfiguration configuration)
         {
             _context = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
         }
 
-
         // GET: api/<ReviewsController>
-  //      [HttpGet]
-  //      public async Task<IActionResult> GetALLMemberFavorite()
-  //      {
-  //          const string sql = @"SELECT * FROM [Reviews]";
+        [HttpGet]
+        public async Task<IActionResult> GetALLReviews()
+        {
+            const string sql = @"SELECT * FROM [Reviews]";
 
-  //          using (var multi = await _context.QueryMultipleAsync(sql))
-  //          {
-  //              var Reviewss = multi.Read<Reviews>().ToList();
-  //              // 基本驗證，確保資料存在
-  //              if (Reviewss == null)
-  //              {
-  //                  return NotFound("No data found");
-  //              }
-  //              return Ok(Reviewss);
-  //          }
-  //      }
+            using (var multi = await _context.QueryMultipleAsync(sql))
+            {
+                var reviews = multi.Read<Review>().ToList();
 
-  //      SELECT
-  //  r.ReviewID,
-  //  r.MemberID,
-  //  r.ProductID,
-  //  r.ClassID,
-  //  r.Rating,
-  //  r.Comment,
-  //  r.ReviewDateTime,
-  //  r.TrainerID,
-  //  r.ClassTypeID,
+                if (!reviews.Any())
+                {
+                    return NotFound("No data found");
+                }
 
-  //  m.MemberID,
-  //  m.MemberName,
-
-  //  p.ProductID,
-  //  p.ProductName,
-
-  //  c.ClassID,
-  //  c.CourseStatus,
-
-  //  t.TrainerID,
-  //  t.TrainerName,
-
-  //  ct.ClassTypeID,
-  //  ct.ClassName
-
-  //FROM Reviews as r
-
-  //LEFT JOIN[Member] as m ON r.MemberID = m.MemberID
-
-  //LEFT JOIN Product as p ON r.ProductID = p.ProductID
-
-  //LEFT JOIN Class as c ON r.ClassID = c.ClassID
-
-  //LEFT JOIN Trainers as t ON r.TrainerID = t.TrainerID
-
-  //LEFT JOIN ClassTypes as ct ON r.ClassTypeID = ct.ClassTypeID
-
-
-  //WHERE r.MemberID = 3;
-
-
+                return Ok(reviews);
+            }
+        }
 
         // GET api/<ReviewsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetReviews(int id)
         {
-            return "value";
+            const string sql = @"
+                SELECT
+                    r.ReviewID, r.MemberID, r.ProductID, r.ClassID, r.Rating, r.Comment, r.ReviewDateTime, r.TrainerID, r.ClassTypeID,
+                    m.MemberID, m.MemberName,
+                    p.ProductID, p.ProductName,
+                    c.ClassID, c.CourseStatus,
+                    t.TrainerID, t.TrainerName,
+                    ct.ClassTypeID, ct.ClassName
+                FROM Reviews as r
+                LEFT JOIN [Member] as m ON r.MemberID = m.MemberID
+                LEFT JOIN Product as p ON r.ProductID = p.ProductID
+                LEFT JOIN Class as c ON r.ClassID = c.ClassID
+                LEFT JOIN Trainers as t ON r.TrainerID = t.TrainerID
+                LEFT JOIN ClassTypes as ct ON r.ClassTypeID = ct.ClassTypeID
+                WHERE r.MemberID = @MemberId";
+
+            var parameters = new { MemberId = id };
+
+            var reviewsResult = await _context.QueryAsync<Review, Member, Product, Class, Trainer, ClassType, Review>(
+                sql,
+                (review, member, product, classObj, trainer, classType) =>
+                {
+                    if (member != null)
+                        review.Members.Add(member);
+                    if (product != null)
+                        review.Products.Add(product);
+                    if (classObj != null)
+                        review.Classs.Add(classObj);
+                    if (trainer != null)
+                        review.Trainers.Add(trainer);
+                    if (classType != null)
+                        review.ClassTypes.Add(classType);
+
+                    return review;
+                },
+                param: parameters,
+                splitOn: "MemberId,ProductId,ClassId,TrainerId,ClassTypeId"
+            );
+
+            if (!reviewsResult.Any())
+            {
+                return NotFound("No data found");
+            }
+
+            return Ok(new { reviewsWithMembers = reviewsResult });
         }
 
-        // POST api/<ReviewsController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        //// POST api/<ReviewsController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
 
-        // PUT api/<ReviewsController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //// PUT api/<ReviewsController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
-        // DELETE api/<ReviewsController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/<ReviewsController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
--- FitMatch-API/Models/Review.cs
+++ FitMatch-API/Models/Review.cs
@@ -1,26 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
-using System.ComponentModel;
-using System.Security.Claims;
+﻿using System;
+using System.Collections.Generic;
 
 namespace FitMatch_API.Models
 {
-
     public partial class Review
     {
         public int ReviewId { get; set; }
-
         public int? MemberId { get; set; }
-
         public int? ProductId { get; set; }
-
         public int? ClassId { get; set; }
-
         public int? Rating { get; set; }
-
         public string? Comment { get; set; }
-
         public DateTime? ReviewDateTime { get; set; }
         public string? MemberName { get; set; }
-
+        public string? ProductName { get; set; }
+        public string CourseStatus { get; set; }
+        public int TrainerId { get; set; }
+        public string? TrainerName { get; set; }
+        public int ClassTypeId { get; set; }
+        public string? ClassName { get; set; }
+        public List<Trainer> Trainers { get; set; } = new List<Trainer>();
+        public List<Product> Products { get; set; } = new List<Product>();
+        public List<Member> Members { get; set; } = new List<Member>();
+        public List<Class> Classs { get; set; } = new List<Class>();
+        public List<ClassType> ClassTypes { get; set; } = new List<ClassType>();
     }
 }
